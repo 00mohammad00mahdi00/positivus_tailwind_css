eb1b0682592fa72a1f39217d80c9804524fffdc0
mohammad mahdi ganjalia <mohammad00mahdi00ganji@gmail.com>
2025-01-27 12:49:26 +0330

    complete is section main in the file index.html

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -47,3 +47,123 @@ menuBar.addEventListener("click", function(){
 
     }
 })
+
+const workingProcessCaer1 = $.getElementById("workingProcessCaer1")
+
+const workingProcessBtn1 = $.getElementById("workingProcessBtn1")
+
+const workingProcessContent1 =$.getElementById("workingProcessContent1")
+
+const workingProcessIcon1 = $.getElementById("workingProcessIcon1")
+
+
+const workingProcessCaer2 = $.getElementById("workingProcessCaer2")
+
+const workingProcessBtn2 = $.getElementById("workingProcessBtn2")
+
+const workingProcessContent2 =$.getElementById("workingProcessContent2")
+
+const workingProcessIcon2 = $.getElementById("workingProcessIcon2")
+
+
+const workingProcessCaer3 = $.getElementById("workingProcessCaer3")
+
+const workingProcessBtn3 = $.getElementById("workingProcessBtn3")
+
+const workingProcessContent3 =$.getElementById("workingProcessContent3")
+
+const workingProcessIcon3 = $.getElementById("workingProcessIcon3")
+
+
+const workingProcessCaer4 = $.getElementById("workingProcessCaer4")
+
+const workingProcessBtn4 = $.getElementById("workingProcessBtn4")
+
+const workingProcessContent4 =$.getElementById("workingProcessContent4")
+
+const workingProcessIcon4 = $.getElementById("workingProcessIcon4")
+
+
+const workingProcessCaer5 = $.getElementById("workingProcessCaer5")
+
+const workingProcessBtn5 = $.getElementById("workingProcessBtn5")
+
+const workingProcessContent5 =$.getElementById("workingProcessContent5")
+
+const workingProcessIcon5 = $.getElementById("workingProcessIcon5")
+
+
+const workingProcessCaer6 = $.getElementById("workingProcessCaer6")
+
+const workingProcessBtn6 = $.getElementById("workingProcessBtn6")
+
+const workingProcessContent6 =$.getElementById("workingProcessContent6")
+
+const workingProcessIcon6 = $.getElementById("workingProcessIcon6")
+
+function WorkingProcessOpen(card, btn, content, img){
+    
+
+    let flag = true
+    btn.addEventListener("click", function(){
+        if(flag){
+            img.src = "./images/index/svg/icon_substraction.svg"
+
+            card.classList.remove("bg-third")
+            card.classList.add("bg-primary")
+
+            content.classList.remove("hidden")
+            content.classList.add("block")
+
+            flag = false
+        }else{
+            img.src = "./images/index/svg/icon_Addition.svg"
+
+            card.classList.add("bg-third")
+            card.classList.remove("bg-primary")
+
+            content.classList.remove("block")
+            content.classList.add("hidden")
+
+            flag = true
+        }
+    })
+}
+
+
+function WorkingProcessClose(card, btn, content, img){
+    
+
+    let flag = true
+    btn.addEventListener("click", function(){
+        if(flag){
+            img.src = "./images/index/svg/icon_Addition.svg"
+
+            card.classList.add("bg-third")
+            card.classList.remove("bg-primary")
+
+            content.classList.remove("block")
+            content.classList.add("hidden")
+
+            flag = false
+        }else{
+            
+            img.src = "./images/index/svg/icon_substraction.svg"
+
+            card.classList.remove("bg-third")
+            card.classList.add("bg-primary")
+
+            content.classList.remove("hidden")
+            content.classList.add("block")
+
+            flag = true
+        }
+    })
+}
+
+WorkingProcessClose(workingProcessCaer1,workingProcessBtn1,workingProcessContent1,workingProcessIcon1)
+WorkingProcessOpen(workingProcessCaer2,workingProcessBtn2,workingProcessContent2,workingProcessIcon2)
+WorkingProcessOpen(workingProcessCaer3,workingProcessBtn3,workingProcessContent3,workingProcessIcon3)
+WorkingProcessOpen(workingProcessCaer4,workingProcessBtn4,workingProcessContent4,workingProcessIcon4)
+WorkingProcessOpen(workingProcessCaer5,workingProcessBtn5,workingProcessContent5,workingProcessIcon5)
+WorkingProcessOpen(workingProcessCaer6,workingProcessBtn6,workingProcessContent6,workingProcessIcon6)
